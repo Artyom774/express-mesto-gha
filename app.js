14a3f88221dc22e84a501f7f1f81ac60a51b0b6b
ArtyomVoloshin <artyom-first@yandex.ru>
2022-09-26 22:05:00 +0500

    работающий мангуст

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/users.js');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  /*useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false*/
+});
 
 const timeLog = (req, res, next) => {
   console.log(req.method);
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,33 @@
 const router = require('express').Router();
-const { users } = require('../db.js');
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
-  if (users.length === 0) {
-    res.send(`База пользователей пуста`);
-    return;
-  }
-  res.send(`Список пользователей:
-  ${users}`);
+  User.find({})
+    .then(users => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 router.get('/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.send(user))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+});
+
+/*router.get('/:id', (req, res) => {
   if (!users[req.params.id]) {
     res.send(`Такого пользователя не существует`);
     return;
   }
   const { name, age } = users[req.params.id];
   res.send(`Пользователь ${name}, ${age} лет`);
+});*/
+
+router.post('/', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then(user => res.send( user ))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 module.exports = router;
