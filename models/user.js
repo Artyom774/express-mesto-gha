599c51e93bd5910c3482262b218ff4ccbc273d87
Artyom <artyom-first@yandex.ru>
2022-10-03 19:45:10 +0500

    feat: настроено создание пользователя

--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { // у пользователя есть имя — опишем требования к имени в схеме:
     type: String, // имя — это строка
-    required: true, // оно должно быть у каждого пользователя
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
+    default: 'Жак-Ив Кусто',
   },
   email: {
     type: String,
@@ -20,13 +20,13 @@ const userSchema = new mongoose.Schema({
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 userSchema.set('versionKey', false);
