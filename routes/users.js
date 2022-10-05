92a601e2a518af56281a75f179d5c938fa0b8b96
Artyom <artyom-first@yandex.ru>
2022-10-05 22:35:15 +0500

    feat: настроен celebarste

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,21 @@ const {
 
 usersRouter.get('/', findAllUsers);
 usersRouter.get('/:id', findUserById);
-usersRouter.post('/signin', login);
-usersRouter.post('/signup', createUser);
+usersRouter.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(5),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+usersRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(8),
+    email: Joi.string().required().min(5),
+    password: Joi.string().required().min(8),
+    about: Joi.string().min(2).max(8),
+    avatar: Joi.string(),
+  }),
+}), createUser);
 usersRouter.patch('/me', updateUser);
 usersRouter.patch('/me/avatar', updateAvatar);
 
