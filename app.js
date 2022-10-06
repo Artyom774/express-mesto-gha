b5b51a8cd60bcc1d66697abe50b5058a9d9ee949
Artyom <artyom-first@yandex.ru>
2022-10-06 16:00:24 +0500

    fix: изменены роуты для регистрации и авторизации

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
+const {
+  createUser, login,
+} = require('./controllers/users');
 
 const { PORT = 3000 } = process.env; // файла .env нет в проекте
 const app = express();
@@ -23,9 +28,29 @@ app.use(timeLog);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+usersRouter.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(5),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+usersRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(8),
+    email: Joi.string().required().min(5),
+    password: Joi.string().required().min(8),
+    about: Joi.string().min(2).max(8),
+    avatar: Joi.string(),
+  }),
+}), createUser);
 app.use('/users', usersRouter);
 app.use('/cards', auth);
 app.use('/cards', cardsRouter);
+app.use(errors());
+app.use((err, req, res, next) => {
+  res.status(400).send({ message: err.message });
+  next();
+});
 app.use('/', (req, res) => { res.status(404).send({ message: 'Неправильный url-адрес запроса' }); });
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,10 @@
 const usersRouter = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const {
-  findAllUsers, findUserById, createUser, updateUser, updateAvatar, login,
+  findAllUsers, findUserById, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 usersRouter.get('/', findAllUsers);
 usersRouter.get('/:id', findUserById);
-usersRouter.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().min(5),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-usersRouter.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(8),
-    email: Joi.string().required().min(5),
-    password: Joi.string().required().min(8),
-    about: Joi.string().min(2).max(8),
-    avatar: Joi.string(),
-  }),
-}), createUser);
 usersRouter.patch('/me', updateUser);
 usersRouter.patch('/me/avatar', updateAvatar);
 
