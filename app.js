56300a88be4fd8839eed6b1856469401616bbf5b
Artyom <artyom-first@yandex.ru>
2022-10-08 11:17:16 +0500

    feat: регистрация и авторизация вынесены в отдельные маршруты

--- app.js
+++ app.js
@@ -3,56 +3,39 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const signInRouter = require('./routes/signIn');
+const signUpRouter = require('./routes/signUp');
 const auth = require('./middlewares/auth');
-const {
-  createUser, login,
-} = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
 
-const URLregex = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
-
 const { PORT = 3000 } = process.env; // файла .env нет в проекте
-const app = express();
+const app = express(); // app работает через фреймворк Express
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', { // подключение к базе MongooseDB
   useNewUrlParser: true,
 });
 
-const timeLog = (req, res, next) => {
+const timeLog = (req, res, next) => { // вывод метода запроса
   console.log('Используемый метод запроса: ', req.method);
   next();
 };
 
-app.use(timeLog);
+app.use(timeLog); // при любом запросе выводит его метод (нужно для разработки)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().min(5),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(8),
-    email: Joi.string().required().min(5).email(),
-    password: Joi.string().required().min(8),
-    about: Joi.string().min(2).max(8),
-    avatar: Joi.string().pattern(URLregex),
-  }),
-}), createUser);
+app.post('/signin', signInRouter); // авторизация пользователя
+app.post('/signup', signUpRouter); // регистрация пользователя
 app.use(auth); // проверка токена
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-app.use('/', (req, res) => { res.status(404).send({ message: 'Неправильный url-адрес запроса' }); });
-app.use(errors());
-app.use(errorHandler);
+app.use('/users', usersRouter); // пути для работы с карточками
+app.use('/cards', cardsRouter); // пути для работы с пользователем
+app.use('/', (req, res) => { res.status(404).send({ message: 'Неправильный url-адрес запроса' }); }); // введён неизвестный путь
+app.use(errors()); // обработка ошибок библиотеки celebrate
+app.use(errorHandler); // обработка ошибок сервера
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { // при запуске сервера выводит порт
   console.log(`Порт сервера: ${PORT}`);
 });
---
+++ routes/signUp.js
@@ -0,0 +1,17 @@
+const signUpRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { createUser } = require('../controllers/users');
+
+const URLregex = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
+
+signUpRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(8),
+    email: Joi.string().required().min(5).email(),
+    password: Joi.string().required().min(8),
+    about: Joi.string().min(2).max(8),
+    avatar: Joi.string().pattern(URLregex),
+  }),
+}), createUser);
+
+module.exports = signUpRouter;
