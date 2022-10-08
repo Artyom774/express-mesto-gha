2f680aeaacb9b18cf7f153c3bb23131f350abdb1
Artyom <artyom-first@yandex.ru>
2022-10-08 11:26:12 +0500

    fix: неправлиьный адрес для регистрации

--- routes/signUp.js
+++ routes/signUp.js
@@ -4,7 +4,7 @@ const { createUser } = require('../controllers/users');
 
 const URLregex = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+/;
 
-signUpRouter.post('/signup', celebrate({
+signUpRouter.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(8),
     email: Joi.string().required().min(5).email(),
